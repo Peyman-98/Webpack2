587a34c8c5f664459f6313bc9a806080d3100d5f
Peyman <ds_worm@yahoo.com>
2017-04-16 16:08:38 +0430

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const glob = require('glob');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var isProd = process.env.NODE_ENV === 'production';
+var cssDev = ['style-loader','css-loader', 'sass-loader'];
+var cssProd = ExtractTextPlugin.extract({
+    fallback: "style-loader",
+    use: ['css-loader', 'sass-loader'],
+    publicPath: './'
+});
+var cssConfig = isProd ? cssProd :cssDev;
+
+module.exports = {
+    entry: {
+       bundle: './src/app.js',
+       vendor: 'jquery'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: cssConfig
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    "file-loader?name=images/[name].[ext]",
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {}
+                    }
+                ]
+            },
+            // {
+            //     test: /\.(ttf|eot|woff|woff2)$/,
+            //     loader: 'file-loader',
+            //     options: {
+            //         name: 'fonts/[name].[ext]',
+            //     },
+            // },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname,"dist"),
+        compress: true,
+        hot: !isProd,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // minify:{
+            //     collapseWhitespace : true
+            // },
+            hash: true,
+            template: './src/index.html',
+        }),
+        new ExtractTextPlugin({
+            filename: "styles.css",
+            disable: !isProd,
+            allChunks: true
+        }),
+        new PurifyCSSPlugin({
+            paths: glob.sync(path.join(__dirname, 'src/*.html')),
+        }),
+        new UglifyJSPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        })
+        // new webpack.ProvidePlugin({
+        //     $: 'jquery',
+        //     jQuery: 'jquery'
+        // })
+    ]
+}
