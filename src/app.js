fb7b38f6ad38dc46fdf355b022c17fd8e08be8c5
Peyman <ds_worm@yahoo.com>
2017-04-17 15:56:26 +0430

    Jquery,Gsap and Scroll Magic added as dependencies

--- src/app.js
+++ src/app.js
@@ -1,15 +1,4 @@
 const css = require('./app.scss');
-// var $ = require("jquery");
 import $ from 'jquery'
-let msg = "Hi There";
-var dd = 'aaaa';
-console.log(msg);
-function multiply(a,b){
-    return a*b;
-}
-$(document).ready(function(){
-    $('.heading').css('background','yellow');
-    console.log(this);
-});
 
 
